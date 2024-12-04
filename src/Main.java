e54a8943e3b103fdc6768d5e0107ebe51976a796
mak1115 <mak1115@swcsd.us>
2024-12-04 12:12:14 -0500

    Changed Project name and added one small change Another small change

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Worlds!");
+        System.out.println("One small change!");
+        System.out.println("Another small change!");
     }
 }
